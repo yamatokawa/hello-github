b869aabdfb75a1dcac4e67905829bca1ddd07e57
yamatokawa <50351057+yamatokawa@users.noreply.github.com>
2019-05-10 16:26:07 +0900

    Add files via upload

---
+++ 1.8.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void)
+{
+	
+	int input=0;
+	int sum=0;
+	int zei=0;
+	
+	printf ("Á”ïÅ”²‚«‰¿Ši‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢.\n");
+	scanf("%d",&input);
+
+	sum=input*1.08;
+	zei=input*0.08;
+
+	printf("Å‚İ‰¿Ši=%d‰~",sum);
+	printf("ÅŠz=%d‰~",zei);
+
+	return 0;
+}
